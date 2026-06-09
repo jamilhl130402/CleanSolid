867a8f9b9271bd53c90cd595e774bffa6331854c
Ruben Jamyl Huarayo Leon <jamilrcsr@gmail.com>
2026-06-09 08:59:24 -0500

    Actualizando archivos

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,52 @@
+(() => {
+
+    // Ejemplo
+    // Archivos a evaluar - files to evaluate
+    const filesToEvaluate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const filesToDelete = filesToEvaluate.map( files => files.flagged );
+
+    
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+   // Fecha actual
+const currentDate = new Date();
+
+// Días transcurridos
+const elapsedTimeInDays: number = 23;
+
+// Número de archivos en el directorio
+const numberOfFilesInDirectory = 33;
+
+// Nombre del usuario
+const firstName = 'Fernando';
+
+// Apellido del usuario
+const lastName = 'Herrera';
+
+// Días desde la última modificación
+const daysSinceLastModification = 12;
+
+// Cantidad máxima de clases por estudiante
+const maxClassesPerStudent = 6;
+
+
+})();
